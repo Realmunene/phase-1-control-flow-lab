d81f2c6f38ca72ed606476665e65897f5be4b1ce
Realmunene <josep.mwangi@student.moringaschool.com>
2022-08-24 09:29:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let journey;
+if(distance <=400){
+    journey = "This is on me!";
+if(distance > 400 && distance <= 2000){
+  journey= 30*distance;
+}
+return journey;
+   }else {
+    journey ="The Ride can not be allowed";
+    return journey;
+   } 
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+return city =='NYC'?'Ok, sounds good.':'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+  default:
+  return 'Bye.';
+    break;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
